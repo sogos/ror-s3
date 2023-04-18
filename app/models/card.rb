512976f3f73420e54ac27f224dc574e7fc6c7073
Thibault Cordier <thibault.cordier@gmail.com>
2023-04-18 16:17:54 +0200

    Exemple to read file before upload

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,17 @@
 class Card < ApplicationRecord
+
+
   validates :title, presence: true
   validates :image, presence: true
   validates :description, presence: true, length: { minimum: 10 }
   has_one_attached :image
+  before_save :analyze_image_metadata
+  def analyze_image_metadata
+    if image.attached?
+      # Analyze the metadata
+      tempfile = attachment_changes['image'].attachable.tempfile
+      image_path = tempfile.path
+      metadata = MiniMagick::Image.new(tempfile.path)
+    end
+  end
 end
